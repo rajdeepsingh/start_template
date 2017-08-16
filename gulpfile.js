06ecfd41667621ff107ed3946f1c986ecb1bb19d
BoldPC/Rajdeep Singh <singhinthehouse@gmail.com>
2017-08-15 20:49:22 -0500

    added an updated gulpfile.js and a better file structure for sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     webserver = require('gulp-webserver');
 
-gulp.task('watch', function() {
-  gulp.watch('assets/js/**/*.js');
-  gulp.watch(['assets/css/**/*.css']);
-  gulp.watch('*.html');
-});
+    gulp.task('sass', function() {
+      return gulp.src('./source/styles/styles.sass')
+              .pipe(sourcemaps.init())
+              .pipe(sass(outputStyle: 'expanded').on('error', function() {
+                console.log('There was an error processing the sass');
+              }))
+              .pipe(sourcemaps.write())
+              .pipe(gulp.dest('./build/styles/styles.css'));
+    });
 
-gulp.task('webserver', function() {
-    gulp.src('')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
+    gulp.task('watch', function() {
+      gulp.watch('build/js/**/*', ['js']);
+      gulp.watch('source/styles/*');
+    });
 
-gulp.task('default', ['watch','webserver']);
+    gulp.task('webserver', function() {
+      gulp.src('build/')
+      .pipe(webserver({
+        livereload: true,
+        open: true
+      }));
+    });
+
+    gulp.task('default', ['watch', 'sass', 'webserver']);
