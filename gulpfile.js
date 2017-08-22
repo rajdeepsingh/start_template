1858d36e526647367f9d32fc15f6afe46b7b09ee
BoldPC/Rajdeep Singh <singhinthehouse@gmail.com>
2017-08-22 11:50:53 -0500

    final starter template with sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     webserver = require('gulp-webserver');
 
-gulp.task('watch', function() {
-  gulp.watch('assets/js/**/*.js');
-  gulp.watch(['assets/css/**/*.css']);
-  gulp.watch('*.html');
-});
+    gulp.task('sass', function() {
+      return gulp.src('./source/styles/styles.scss')
+              .pipe(sourcemaps.init())
+              .pipe(sass({outputStyle: 'expanded'}).on('error', function(e) {
+                console.log('There was an error processing the sass' + e);
+              }))
+              .pipe(sourcemaps.write())
+              .pipe(gulp.dest('./build/styles'));
+    });
 
-gulp.task('webserver', function() {
-    gulp.src('')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
+    gulp.task('watch', function() {
+      gulp.watch('build/scripts/**/*');
+      gulp.watch(['source/styles/**/*'], ['sass']);
+    });
 
-gulp.task('default', ['watch','webserver']);
+    gulp.task('webserver', function() {
+      gulp.src('build/')
+      .pipe(webserver({
+        livereload: true,
+        open: true
+      }));
+    });
+
+    gulp.task('default', ['watch', 'sass', 'webserver']);
